4a2e79be56f7cac1df32dc66997718f2deed4873
Timothy Johnson <timothy@wearefound.com>
2013-04-17 14:53:13 -0400

    initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestCedric::Application.config.secret_token = '7d09fd8bfa59265d554f071e54957f5ce9cf6a1e1d179a61a684618b5b7f600ea9ceda9ffc7107b33e4433cc2d1cdb2c2c27187ab79d43ea32498a4d1e1df28c'
